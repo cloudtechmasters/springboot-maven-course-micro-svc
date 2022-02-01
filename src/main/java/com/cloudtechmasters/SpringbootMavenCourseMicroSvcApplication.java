f62516c93dd2306892b3b8aaf2a5e3ceeff7214b
cloudtechmasters <68885738+cloudtechmasters@users.noreply.github.com>
2022-02-01 19:24:00 +0530

    Update SpringbootMavenCourseMicroSvcApplication.java

--- src/main/java/com/cloudtechmasters/SpringbootMavenCourseMicroSvcApplication.java
+++ src/main/java/com/cloudtechmasters/SpringbootMavenCourseMicroSvcApplication.java
@@ -1,15 +1,34 @@
 package com.cloudtechmasters;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
+@EnableScheduling
 @SpringBootApplication
 @ComponentScan(basePackages = "com.cloudtechmasters")
 public class SpringbootMavenCourseMicroSvcApplication {
-
+private final static Logger log = LoggerFactory.getLogger(SpringbootMavenCourseMicroSvcApplication.class);
+	@Autowired
+    private ApplicationContext applicationContext;
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootMavenCourseMicroSvcApplication.class, args);
 	}
-
+@Scheduled(fixedRate = 5000)
+    public void log() {
+        log.info("This is \na multiline\n\n\nlog");
+    }
+    private void m() {
+    }
+    @Scheduled(fixedRate = 10000)
+    public void logException() {
+        try {
+            applicationContext.getBean("test");
+        } catch (Exception e) {
+            throw new RuntimeException("Error happened", e);
+        }
+    }
 }

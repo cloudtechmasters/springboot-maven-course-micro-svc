a7e94500462e727604365f1d0cfa78e8e8ae45c1
cloudtechmasters <68885738+cloudtechmasters@users.noreply.github.com>
2022-08-06 23:50:55 -0400

    Update CourseController.java

--- src/main/java/com/cloudtechmasters/CourseController.java
+++ src/main/java/com/cloudtechmasters/CourseController.java
@@ -5,13 +5,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 @RestController
 public class CourseController {
+    private static final Logger logger = LoggerFactory.getLogger(CourseController.class);
 //create two endpoints getAllAwsServices and getAllDevopsTools
     @GetMapping("/getAllAwsServices")
     public List<String> getAllAwsServices(){
-        System.out.println("added for failing sonar build");
+        logger.info("added for failing sonar build");
         return Arrays.asList("ec2","iam","rds","ecr","eks");
     }
 

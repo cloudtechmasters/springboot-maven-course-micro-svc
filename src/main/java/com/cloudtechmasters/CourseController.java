c7252883d44c2fce42db88e3c4aba0e4c1a98852
yaswanthe <awsyasu47@gmail.com>
2023-11-29 11:46:21 +0530

    coad changed

--- src/main/java/com/cloudtechmasters/CourseController.java
+++ src/main/java/com/cloudtechmasters/CourseController.java
@@ -19,6 +19,6 @@ public class CourseController {
 
     @GetMapping("/getAllDevopsTools")
     public List<String> getAllDevopsTools(){
-        return Arrays.asList("chintu java project deploymentversion-1");
+        return Arrays.asList("netstat -tulnp java project deploymentversion-1");
     }
 }

07e5583eee954d9375104367bc6c5d17828f9ba8
cloudtechmasters <68885738+cloudtechmasters@users.noreply.github.com>
2024-01-15 11:53:12 -0500

    Update CourseController.java

--- src/main/java/com/cloudtechmasters/CourseController.java
+++ src/main/java/com/cloudtechmasters/CourseController.java
@@ -13,7 +13,7 @@ public class CourseController {
 //create two endpoints getAllAwsServices and getAllDevopsTools
     @GetMapping("/getAllAwsServices")
     public List<String> getAllAwsServices(){
-        logger.info("added for failing sonar build");
+        System.out.println("added for failing sonar build");
         return Arrays.asList("ec2","iam","rds","ecr","eks");
     }
 
